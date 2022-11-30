91d6143d5811c91112afaea00576edbbb2914c00
M7ilan <81147447+M7ilan@users.noreply.github.com>
2022-11-30 20:35:41 +0300

    Update Anti AFK.lua

--- Anti AFK.lua
+++ Anti AFK.lua
@@ -1,6 +1,8 @@
 local VU = game:GetService("VirtualUser")
-game:GetService("Players").LocalPlayer.Idled:connect(function()
-   VU:Button2Down(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
-   task.wait(1)
-   VU:Button2Up(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
+local LP = game:GetService("Players").LocalPlayer
+
+LP.Idled:Connect(function()
+    VU:Button2Down(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
+    task.wait(1)
+    VU:Button2Up(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
 end)
