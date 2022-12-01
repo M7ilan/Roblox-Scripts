e003948e3e864d99ee78006049585e1f090667ce
M7ilan <81147447+M7ilan@users.noreply.github.com>
2022-12-01 18:11:15 +0300

    Create Save Specific Instance.lua

---
+++ Save Specific Instance.lua
@@ -0,0 +1,2 @@
+_G.ToSave = game:GetService("CoreGui").Orion -- Put the path of the thing you want to save here (you can copy paths with dex)
+loadstring(game:HttpGet(('https://raw.githubusercontent.com/Aidez/MISC/main/save_specific_instance'),true))()
